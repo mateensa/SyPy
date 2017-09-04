c9f6b3d5f2b3a7f8ffdeff9692b4229cdbc73ddf
Mateen SA <samateen.sb@gmail.com>
2017-09-04 17:22:12 +0530

    Add files via upload

---
+++ InsertionSort.py
@@ -0,0 +1,26 @@
+#Name: SelectionSort
+#Author: Mateen SA
+#Comments: It always maintains a sorted sublist in the lower positions of the list.
+            # Each new item is then “inserted” back into the previous sublist such that the sorted sublist is one item larger.
+
+
+def insertionSort(alist):
+
+    for index in range(1, len(alist)):
+
+        startPosition = index;
+        currentValue = alist[startPosition];
+
+        while startPosition > 0 and alist[startPosition-1] > currentValue:
+
+            #Always take currentValue and check in sublist (that is, from 0 to start position in alist)
+            alist[startPosition] = alist[startPosition -1 ];
+            startPosition = startPosition - 1;
+
+        alist[startPosition] = currentValue;
+
+
+
+alist = [54,26,93,17,77,31,44,55,20]
+insertionSort(alist)
+print(alist)
---
+++ SelectionSort.py
@@ -0,0 +1,25 @@
+#Name: SelectionSort
+#Author: Mateen SA
+#Comments: Its is very similar to Bubble sort except it exchanges max item to max position.
+
+def SelectionSort(alist):
+
+
+    for fillslot in range(len(alist)-1, 0 , -1):
+
+        #Checking from length-1 in descending order to replace max value to max position
+        #unlike Bubble Sort
+
+        positionOfMax = 0;
+        for location in range(1, fillslot+1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location;
+
+            temp = alist[positionOfMax];
+            alist[positionOfMax] = alist[fillslot];
+            alist[fillslot] = temp;
+
+
+li = [54,26,93,17,77,31,44,55,20];
+SelectionSort(li);
+print(li)
