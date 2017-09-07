a6d7eb9c2eb177b5f7fa65124500df82348e174b
Mateen SA <samateen.sb@gmail.com>
2017-09-07 09:14:45 +0530

    Update Test_UDF.py

--- psycogp2_test/Test_UDF.py
+++ psycogp2_test/Test_UDF.py
@@ -1,7 +1,7 @@
 import psycopg2;
 retval ="";
 try:
-	con=psycopg2.connect("dbname=dwa user=integration_user host=logidwa.cmj8jk61oxi0.us-west-2.redshift.amazonaws.com password=Logitech123 port=5439");
+	con=psycopg2.connect("dbname=dwa user=integration_user host=xxxxxx.cmj8jk61oxi0.us-west-2.redshift.amazonaws.com password= port=5439");
 	retval = "I am able to connect to the database";
 except:
     retval = "---->I am unable to connect to the database";
