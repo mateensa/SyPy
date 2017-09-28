d87db717b9f2ad825f8e8d3a8e7a871059336056
Mateen SA <samateen.sb@gmail.com>
2017-09-29 00:04:51 +0530

    Delete Test_UDF.py

--- psycogp2_test/Test_UDF.py
+++
@@ -1,9 +0,0 @@
-import psycopg2;
-retval ="";
-try:
-	con=psycopg2.connect("dbname=dwa user=integration_user host=xxxxxx.cmj8jk61oxi0.us-west-2.redshift.amazonaws.com password= port=5439");
-	retval = "I am able to connect to the database";
-except:
-    retval = "---->I am unable to connect to the database";
-
-print(retval)
